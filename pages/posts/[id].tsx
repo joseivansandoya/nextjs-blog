f83a7b01ae5985347aab8ed6a58bcfbc28d569a8
Jose Sandoya <joseivansandoya@gmail.com>
2022-07-10 16:29:13 -0500

    migrate to TS

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
 import Head from 'next/head';
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
@@ -7,7 +8,7 @@ import Layout from '../../components/layout';
 
 export default function Post({ postData }) {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>{postData.title}</title>
       </Head>
@@ -22,7 +23,7 @@ export default function Post({ postData }) {
   );
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -30,7 +31,7 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id);
   return {
     props: {
